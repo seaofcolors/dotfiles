c91d7baf1336997e88b9391cc90097c6843e673f
seaofcolors <techanditemail@pm.me>
2026-04-17 10:52:05 +0200

    Update .config/hypr/scripts/xdg-fix.sh

--- private_dot_config/hypr/scripts/executable_xdg-fix.sh
+++ private_dot_config/hypr/scripts/executable_xdg-fix.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 sleep 1
-killall -e xdg-desktop-portal-hyprland
-killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
-/usr/lib/xdg-desktop-portal &
+#killall -e xdg-desktop-portal-hyprland
+#killall xdg-desktop-portal
+#/usr/lib/xdg-desktop-portal-hyprland &
+#sleep 2
+#/usr/lib/xdg-desktop-portal &
+
+/usr/lib/xdg-desktop-portal --replace --verbose
