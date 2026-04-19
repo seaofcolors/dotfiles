2f887a74242ba9ddcaabb48205d2b95b1a1adda2
seaofcolors <techanditemail@pm.me>
2026-04-19 11:37:31 +0200

    Update .config/hypr/scripts/waybar-fix.sh

--- private_dot_config/hypr/scripts/executable_waybar-fix.sh
+++ private_dot_config/hypr/scripts/executable_waybar-fix.sh
@@ -2,9 +2,16 @@
 
 sleep 4
 
-if ! pgrep waybar > /dev/null
+#if ! pgrep waybar > /dev/null
+#then
+#  waybar &
+#fi
+
+
+if pgrep waybar | grep "[0-9]" 
 then
+  echo "no"
+else
+  echo "yes"
   waybar &
 fi
-
-
