f070c847e946a957b306b4e5f24ec31cd5df251f
seaofcolors <techanditemail@pm.me>
2026-05-29 16:07:26 +0200

    Update .config/hypr/configs/autostart Update .config/hypr/scripts/insync-fix.sh

--- private_dot_config/hypr/scripts/executable_insync-fix.sh
+++ private_dot_config/hypr/scripts/executable_insync-fix.sh
@@ -4,10 +4,11 @@ sleep 14
 
 if pgrep -x "insync" > /dev/null
 then
-    echo "Insync is running"
+    info "Insync is already running"
 else
     echo "Starting insync"
     QT_QPA_PLATFORM=xcb insync start &
+    info "Insync wasn't running, starting now"
     now=`date +"%x %X"`
     echo "Insync fix executed, ${now}." >> ~/.config/hypr/scripts/mylog.log
 fi
