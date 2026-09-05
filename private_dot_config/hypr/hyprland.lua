4fdd5dcd81fc066b39345c27b5763031f2723d85
seaofcolors <techanditemail@pm.me>
2026-09-06 00:17:11 +0200

    Update .config/hypr/hyprland.lua

--- private_dot_config/hypr/hyprland.lua
+++ private_dot_config/hypr/hyprland.lua
@@ -209,7 +209,7 @@ hl.config({
 -- New tile layout settings
 hl.config({
     layout = {
-        single_window_aspect_ratio = { 3, 3 },
+        single_window_aspect_ratio = { 0, 0 },
         single_window_aspect_ratio_tolerance = 0.1,
     },
     dwindle = {
