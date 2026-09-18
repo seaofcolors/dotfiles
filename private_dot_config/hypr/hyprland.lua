c0979f805b6008d1b9cd8b2206a18a5294c2ef52
seaofcolors <techanditemail@pm.me>
2026-09-18 11:29:53 +0200

    Update .config/hypr/hyprland.lua

--- private_dot_config/hypr/hyprland.lua
+++ private_dot_config/hypr/hyprland.lua
@@ -636,6 +636,7 @@ hl.on("hyprland.start", function()
     -- hl.exec_cmd("wait 5; ~/.config/hypr/scripts/waybar-launch.sh &")
     -- hl.exec_cmd("wait 10; ~/.config/hypr/scripts/waybar-fix.sh &")
 
+    hl.exec_cmd("hyprpaper")
     -- hl.exec_cmd("dunst")
     hl.exec_cmd("swaync")
     hl.exec_cmd("swayosd-server")
@@ -643,8 +644,8 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("udiskie --appindicator")
     hl.exec_cmd("nm-applet &")
     hl.exec_cmd("blueman-applet")
-    hl.exec_cmd("awww-daemon")
-    hl.exec_cmd("awww img --resize stretch ~/.config/backgrounds/raised-fist-3.png")
+--    hl.exec_cmd("awww-daemon")
+--    hl.exec_cmd("awww img --resize stretch ~/.config/backgrounds/raised-fist-3.png")
 
     -- Attempt hypermondcfgd start and fix
     hl.exec_cmd("~/.config/hypr/scripts/hypermoncfgd-fix.sh &")
